61bce1267fe3ed8285c2c65bc4d8a2b4acf583a3
root <root@ip-172-31-18-67.ec2.internal>
2026-05-18 04:22:34 +0000

    Update message v2

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+ifrom flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/health')
@@ -7,7 +7,7 @@ def health():
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Secure GitOps Pipeline Running!"})
+    return jsonify({"message": "Secure GitOps Pipeline v2 - ArgoCD Deployed!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
